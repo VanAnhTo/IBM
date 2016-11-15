ab159a433e6eacced48c428f29cef26e2788f3d8
anhttv14 <vananhto>
2016-11-15 17:42:25 +0700

    Bổ sung cập nhật các element trong task detail

--- src/test/java/page/TaskDetailPage.java
+++ src/test/java/page/TaskDetailPage.java
@@ -1,7 +1,6 @@
 package page;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,14 +17,32 @@ public class TaskDetailPage {
 	@FindBy(css = ".Column.leftColumn tbody tr:nth-child(8) div.ValueHolder.ViewBorder")
 	private WebElement dropDownOwnedBy;
 
-	@FindBy(css = "div.SelectOptions ul li:nth-child(2)")
+	@FindBy(css = "div.com-ibm-team-workitem-web-ui-internal-view-editor-mvvm-views-QueryableComboView-DropDown.ViewBorder.PopUp.Filterable div.SelectOptions ul li:nth-child(2)")
 	private WebElement ownedBy;
-	
+
 	@FindBy(css = ".EstimateWidget2 .com-ibm-team-apt-web-ui-internal-parts-DurationWidget")
 	private WebElement estimateFeild;
-		
+
 	@FindBy(css = "input.input")
 	private WebElement txtEstimate;
+
+	@FindBy(css = "div.DatePicker a:nth-child(3)")
+	private WebElement iconCalendarDueDate;
+
+	@FindBy(css = "div.com-ibm-team-workitem-web-ui-internal-view-mvvm-views-DateTimePopup.Shadow div a.OkButton")
+	private WebElement btnOkDueDate;
+	
+	@FindBy(css = "div.ValueHolder.ViewBorder")
+	private WebElement cbxTaskGroupOnTabTimeTracking;
+	
+	@FindBy(css = "div.SelectOptions ul li:nth-child(11)")
+	private WebElement taskGroupOnTabTimeTracking;
+	
+	@FindBy(css = "div.TabArea a:nth-child(5)")
+	private WebElement tabTimeTracking;
+	
+	@FindBy(css = "span#Timecode_addButton span a span:nth-child(2)")
+	private WebElement linkTextTimeEntryRow;
 
 	public void chooseOwnedBy() {
 		dropDownOwnedBy.click();
@@ -33,12 +50,19 @@ public class TaskDetailPage {
 		ownedBy.click();
 		waitForDropDownOwnedHidden();
 	}
-	
-	public void enterTimeEstimate(String timeEstimate){
+
+	public void chooseDueDate() {
+		iconCalendarDueDate.click();
+		waitForDropDownCalendarDueDateAppear();
+		btnOkDueDate.click();
+		waitForDropDownCalendarDueDateHidden();
+	}
+
+	public void enterTimeEstimate(String timeEstimate) {
 		estimateFeild.click();
 		txtEstimate.sendKeys(timeEstimate);
 	}
-	
+
 	public void enterWith(TaskNameDetail timeEstimate) {
 		this.enterTimeEstimate(timeEstimate.getTimeEstimate());
 	}
@@ -53,5 +77,17 @@ public class TaskDetailPage {
 		util.WaitFor wait = new util.WaitFor(driver);
 		wait.hiddenOfTheElement(By.cssSelector(
 				"div.com-ibm-team-workitem-web-ui-internal-view-editor-mvvm-views-QueryableComboView-DropDown.ViewBorder.PopUp.Filterable"));
+	}
+
+	private void waitForDropDownCalendarDueDateHidden() {
+		util.WaitFor wait = new util.WaitFor(driver);
+		wait.presenceOfTheElement(By
+				.cssSelector("div.com-ibm-team-workitem-web-ui-internal-view-mvvm-views-DateTimePopup.Shadow.Hidden"));
+	}
+
+	private void waitForDropDownCalendarDueDateAppear() {
+		util.WaitFor wait = new util.WaitFor(driver);
+		wait.presenceOfTheElement(
+				By.cssSelector("div.com-ibm-team-workitem-web-ui-internal-view-mvvm-views-DateTimePopup.Shadow"));
 	}
 }
